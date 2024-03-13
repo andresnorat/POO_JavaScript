7c1d08c65d96b4d138126819434b48a53cb12675
Andres Norato <carnorat@gmail.com>
2024-03-13 16:37:11 -0500

    Clases en JavaScript y Patrón RORO

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const andres = {
     ],
 
     aprobarCurso:  function(cursoAprobado){
-        this.cursosAprobados.push(cursoAprobado)
+        this.cursosAprobados.push(cursoAprobado);
     }
 }
 
@@ -23,7 +23,34 @@ function Student(name, age, cursosAprobados){
 }
 
 Student.prototype.aprobarCurso = function(cursoAprobado){
-        this.cursosAprobados.push(cursoAprobado)
+        this.cursosAprobados.push(cursoAprobado);
 }
 
-const juanita = new Student('Juanita', 21, ['Curso de CSS', 'Curso de introduccion a los video juegos'])
+const juanita = new Student('Juanita', 21, ['Curso de CSS', 'Curso de introduccion a los video juegos']);
+
+//Prototipos con la sintaxis de clases
+class Student2 {
+    constructor({
+        name,
+        age,
+        email,
+        cursosAprobados = []
+    }){
+        this.name = name,
+        this.age = age,
+        this.email = email;
+        this.cursosAprobados = cursosAprobados
+    }
+
+
+    aprobarCurso(cursoAprobado){
+        this.cursosAprobados.push(cursoAprobado);
+    }
+}
+
+const miguel =  new Student2({
+    name: "Miguel",
+    age: 25,
+    email: "miguel@gmail.com",
+}
+);
